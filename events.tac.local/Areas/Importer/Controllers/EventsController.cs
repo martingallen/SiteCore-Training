97d58d2523ad3cef28789b2b8f5139ed9447afc9
Martin Allen <martin.allen@hotmail.com>
2018-02-07 14:53:06 +0000

    updating Sitecore content

--- events.tac.local/Areas/Importer/Controllers/EventsController.cs
+++ events.tac.local/Areas/Importer/Controllers/EventsController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using events.tac.local.Areas.Importer.Models;
+using Newtonsoft.Json;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.SecurityModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +17,53 @@ namespace events.tac.local.Areas.Importer.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, string parentPath)
+        {
+            IEnumerable<Event> events = null;
+            string message = null;
+
+            using (var reader = new System.IO.StreamReader(file.InputStream))
+            {
+                var contents = reader.ReadToEnd();
+
+                try
+                {
+                    events = JsonConvert.DeserializeObject<IEnumerable<Event>>(contents);
+                }
+                catch(Exception ex)
+                {
+                    // to be added later
+                    return null;
+                }
+
+                Database database = Sitecore.Configuration.Factory.GetDatabase("master");
+                Item parentItem = database.GetItem(parentPath);
+
+                // ID is the Sitecore ItemId for the Event Details template - this will differ...
+                TemplateID templateID = new TemplateID(new ID("{216DB771-5D7E-4AE7-B686-A4D552C49B27}"));
+                using (new SecurityDisabler())
+                {
+                    foreach (var ev in events)
+                    {
+                        string name = ItemUtil.ProposeValidItemName(ev.ContentHeading);
+                        Item item = parentItem.Add(name, templateID);
+                        item.Editing.BeginEdit();
+                        //assign values for all the fields, for example for ContentHeading:
+                        item["ContentHeading"] = ev.ContentHeading;
+                        item["ContentIntro"] = ev.ContentIntro;
+                        item["Difficulty"] = ev.Difficulty;
+                        item["Duration"] = ev.Duration;
+                        item["Highlights"] = ev.Highlights;
+                        item["StartDate"] = Sitecore.DateUtil.ToIsoDate(ev.StartDate);
+                        item.Editing.EndEdit();
+                    }
+                }
+
+                return View();
+            }
         }
     }
 }
---
+++ events.tac.local/Areas/Importer/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Areas.Importer.Models
+{
+    public class Event
+    {
+        public string ContentHeading { get; set; }
+        public string ContentIntro { get; set; }
+        public string Highlights { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Duration { get; set; }
+        public string Difficulty { get; set; }
+    }
+}
