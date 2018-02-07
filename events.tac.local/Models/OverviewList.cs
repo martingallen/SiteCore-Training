7a3e521d167f1f4098ae8aa2812e24474bdb1dd6
Martin Allen <martin.allen@hotmail.com>
2018-02-07 09:15:42 +0000

    adding navigation controller

---
+++ events.tac.local/Models/OverviewList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local.Models
+{
+    public class OverviewList : List<OverviewItem>
+    {
+        public OverviewList()
+        {
+        }
+    }
+}
