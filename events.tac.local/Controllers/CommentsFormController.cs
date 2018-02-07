97e4eff3f2bb9a847f30796dbea3c10cdb9aaebf
Martin Allen <martin.allen@hotmail.com>
2018-02-07 13:51:28 +0000

    adding two forms, both now react to submission (undesirable)

---
+++ events.tac.local/Controllers/CommentsFormController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Controllers
+{
+    public class CommentsFormController : Controller
+    {
+        // GET: CommentsForm
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string comment, string email)
+        {
+            return View("Confirmation");
+        }
+    }
+}
