1d63570a6b2d91b61d9eb1f7fa6885e29d9ad171
Martin Allen <martin.allen@hotmail.com>
2018-02-07 14:36:18 +0000

    adding importer area

---
+++ events.tac.local/Areas/Importer/Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace events.tac.local.Areas.Importer.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Importer/Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
