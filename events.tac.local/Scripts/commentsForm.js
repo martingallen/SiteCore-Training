ee1e2c6534ff82041cce2c02e83dd999728b77f8
Martin Allen <martin.allen@hotmail.com>
2018-02-07 15:11:50 +0000

    submitting comments using javascript and sitecore API

---
+++ events.tac.local/Scripts/commentsForm.js
@@ -0,0 +1,19 @@
+﻿function createCommentItem(form, path) {
+    var service = new ItemService({ url: '/sitecore/api/ssc/item' });
+    var obj = {
+        ItemName: 'comment - ' + form.email.value,
+        TemplateID: '{6F806945-121E-49CA-B290-AB81E2DD552C}',
+        Name: form.email.value,
+        Comment: form.comment.value
+    }
+    service.create(obj)
+    .path(path)
+    .execute()
+    .then(function (item) {
+        form.email.value = form.comment.value = '';
+        window.alert("Thanks. Your spammy message will appear on the site shortly");
+    })
+    .fail(function (err) { window.alert(err); });
+    event.preventDefault();
+    return false;
+}
